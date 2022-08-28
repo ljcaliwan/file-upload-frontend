bcc4ff190b0c1930a933536e13948c2431bdcd14
ljaycaliwan <caliwanlj27@gmail.com>
2022-08-28 10:04:14 +0800

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Upload from './components/Upload.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {Upload},
+
 }
 </script>
 
